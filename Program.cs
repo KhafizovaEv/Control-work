5f780dfab3b3fee83a81d9cb64bea4dc80ccb90d
Evgeniya <khafizovazheny@gmail.com>
2022-07-04 15:52:23 +0400

    Array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+string[] array1 = new string[6] {"1", "12", "123", "1234", "12345","123456"};
+string[] array2 = new string[array1.Length];
+
