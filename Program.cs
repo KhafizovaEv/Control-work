e0376737d7abb34874d3665098c5886dfeadacbb
Evgeniya <khafizovazheny@gmail.com>
2022-07-04 15:55:41 +0400

    Function Result

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
 string[] array1 = new string[6] {"1", "12", "123", "1234", "12345","123456"};
 string[] array2 = new string[array1.Length];
+void Result(string[] array1, string[] array2)
+{
+    int index = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[index] = array1[i];
+        index++;
+        }
+    }
+}
 
